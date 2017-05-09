04d9889e0e6196896540c156a9fcc798985046d2
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-05-09 14:53:44 +0700

    update navigator

--- navigator.php
+++ navigator.php
@@ -1,6 +1,6 @@
 <ul>
     <li><a href="index.php">Home</a></li>
-    <li><a href="payment.php">Authorize Payment</a></li>
+    <li><a href="authorize-payment.php">Authorize Payment</a></li>
     <li><a href="capture.php">Capture Payment</a></li>
     <li><a href="recurring-payment.php">Recurring Payment</a></li>
     <li><a href="local-payment.php">Local Payment Available</a></li>
