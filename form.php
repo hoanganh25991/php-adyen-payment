52dbfc23e92dedcced8d092178823853577e09cf
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-05-09 10:00:37 +0700

    refactore form with dynamic generationTime

--- form.php
+++ form.php
@@ -1,3 +1,13 @@
+<?php
+
+
+// Adyen base on current time of submit form
+// To validate if request after 24h is invalid
+// Dynamic generate as format "YYYY-MM-DDTHH:mm:ss.sssZ"
+// example: 2017-05-09T19:53:39+07:00
+$generationtime = date(DATE_W3C, mktime(date("H"), date("i"), date("s"), date("m"), date("j"), date("Y")));
+?>
+
 <script type="text/javascript" src="https://test.adyen.com/hpp/cse/js/8214942187553853.shtml"></script>
 <form method="POST" action="" id="adyen-encrypted-form">
     <input type="text" size="20" data-encrypted-name="number" value="5555 4444 3333 1111"/>
@@ -5,7 +15,7 @@
     <input type="text" size="2" data-encrypted-name="expiryMonth" value="08"/>
     <input type="text" size="4" data-encrypted-name="expiryYear" value="2018"/>
     <input type="text" size="4" data-encrypted-name="cvc" value="737"/>
-    <input type="hidden" value="2017-05-08T10:15:00.428+07:00" data-encrypted-name="generationtime"/>
+    <input type="hidden" value="<?php echo $generationtime; ?>" data-encrypted-name="generationtime"/>
     <input type="submit" value="Pay"/>
 </form>
 <script>
--- navigator.php
+++ navigator.php
@@ -1,7 +1,8 @@
 <ul>
     <li><a href="index.php">Home</a></li>
-    <li><a href="payment.php">Authorize payment</a></li>
-    <li><a href="capture.php">Capture payment</a></li>
-    <li><a href="recurring.php">Recurring payment</a></li>
+    <li><a href="payment.php">Authorize Payment</a></li>
+    <li><a href="capture.php">Capture Payment</a></li>
+    <li><a href="recurring.php">Recurring Payment</a></li>
+    <li><a href="local-payment.php">Local Payment Available</a></li>
 </ul>
 <hr/>
